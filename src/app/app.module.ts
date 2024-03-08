d158522a516a05e60b7136795618413a375ef17f
rouaidasahli <rouaidasahli15@gmail.com>
2024-03-08 13:37:03 +0100

    Implement PacksPageComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,29 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InfoComponent } from './info/info.component';
+import { PacksComponent } from './packs/packs.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { FormulaireComponent } from './formulaire/formulaire.component';
+import { EditionsPasseesComponent } from './editions-passees/editions-passees.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoComponent,
+    PacksComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoginComponent,
+    SignUpComponent,
+    AccueilComponent,
+    FormulaireComponent,
+    
+    EditionsPasseesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/packs/packs.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-packs',
+  templateUrl: './packs.component.html',
+  styleUrl: './packs.component.css'
+})
+export class PacksComponent {
+
+}
